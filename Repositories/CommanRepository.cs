b3bbeb7104ac8abc6de0688ed29fb70a88ec2df6
Savan Gangani <savangangani8518@gmail.com>
2024-02-26 14:28:54 +0530

    Intial Commit

---
+++ Repositories/CommanRepository.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace mvc.Repositories
+{
+    public class CommanRepository
+    {
+        public readonly NpgsqlConnection conn;
+         public CommanRepository()
+            {
+                IConfiguration myConfig = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+                                            
+
+                conn = new NpgsqlConnection(myConfig.GetConnectionString("Myconnection"));
+        }
+    }
+}
