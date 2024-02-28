a638ce5b69f1fc2cc38713a33b34eeaaa3c117d9
apitestmail <apitestmail20@gmail.com>
2024-02-28 19:31:49 +0530

    .

--- mvc/obj/Debug/net6.0/mvc.AssemblyInfo.cs
+++ mvc/obj/Debug/net6.0/mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50929df27aae82874e95131ba17f205f2a76204e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
